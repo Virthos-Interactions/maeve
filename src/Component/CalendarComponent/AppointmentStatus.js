a13c1be487287f8d31e100e8cd9597bccc5ad343
Connolyn <matheus_connolyn@hotmail.com>
2021-09-14 20:12:20 -0300

    get colours from appointmentStatus

---
+++ src/Component/CalendarComponent/AppointmentStatus.js
@@ -0,0 +1,29 @@
+module.exports = [
+   {
+        "code": 1,
+        "label":"Pending",
+        "eventBackgroundColor":'#93badb',
+        "eventBorderLeft": '6px solid #525866'
+
+    },
+    {
+        "code":2,
+        "label":"Waiting for payment",
+        "eventBackgroundColor":'#93badb',
+        "eventBorderLeft": '6px solid #525866'
+
+    },
+    {
+        "code":3,
+        "label":"Confirmed",
+        "eventBackgroundColor":'#68aae2',
+        "eventBorderLeft": '6px solid #3867d6'
+    },
+    {
+        "isUndefined":true,
+        "label":"UNDEFINED",
+        "eventBackgroundColor":'#93badb',
+        "eventBorderLeft": '6px solid #525866'
+
+    }
+]
